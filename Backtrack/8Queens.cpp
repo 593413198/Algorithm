0ee91cc02442f1d0f9d412b8905a49d6691ba4f9
161240045-Lu Hao <593413198@qq.com>
2019-05-01 01:09:41 +0800

    update 8Queens

---
+++ Backtrack/8Queens.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int x[8][8]; //0:空  1:有皇后
+int put[8]; //若put[i]=j, 表示第i行j列放着一个皇后
+int check(int i, int j); //检查在(i,j)处放上一个新皇后，是否满足条件
+int putQueen(int ); //表示放置第i行的皇后
+
+
+int check(int i, int j){
+	/* 放上该皇后之前一定是满足条件的
+	 * return 1: 局面满足条件
+	 * return 0: else */
+	for (int )
+}
+
+int main()
+{
+	for (int i)
+	return 1;
+}
